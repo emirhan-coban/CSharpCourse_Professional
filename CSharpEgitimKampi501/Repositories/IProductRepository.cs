cbf2c83cf890f1ad3c09089ca94471acecb8ec22
emirhan-coban <emirhancob4n@gmail.com>
2026-01-04 22:39:37 +0300

    Add project files.

---
+++ CSharpEgitimKampi501/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using CSharpEgitimKampi501.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi501.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<ResultProductDto>> GetAllProductAsync();
+        Task CreateProductAsync(CreateProductDto createProductDto);
+        Task UpdateProductAsync(UpdateProductDto updateProductDto);
+        Task DeleteProductAsync(int productId);
+        Task GetByProductIdAsync(int productId);
+    }
+}
---
+++ CSharpEgitimKampi501/Repositories/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using CSharpEgitimKampi501.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi501.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task CreateProductAsync(CreateProductDto createProductDto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(int productId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<ResultProductDto>> GetAllProductAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task GetByProductIdAsync(int productId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateProductAsync(UpdateProductDto updateProductDto)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
