a47d5c36db43ec76ac8b70e1100437260b50f270
emirhan-coban <emirhancob4n@gmail.com>
2026-01-05 16:20:29 +0300

    dapper calismasi yapildi. query detaylari ogrenildi.

--- CSharpEgitimKampi501/Form1.Designer.cs
+++ CSharpEgitimKampi501/Form1.Designer.cs
@@ -29,20 +29,26 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtProductId = new System.Windows.Forms.TextBox();
             this.btnList = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtProductName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtProductStock = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtProductPrice = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtProductCategory = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblProductTotalCount = new System.Windows.Forms.Label();
+            this.lblMostExpensiveProduct = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblLeastExpensiveProduct = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -56,13 +62,13 @@
             this.label1.Text = "Ürün Id:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox1
+            // txtProductId
             // 
-            this.textBox1.Location = new System.Drawing.Point(96, 36);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtProductId.Location = new System.Drawing.Point(96, 36);
+            this.txtProductId.Multiline = true;
+            this.txtProductId.Name = "txtProductId";
+            this.txtProductId.Size = new System.Drawing.Size(100, 20);
+            this.txtProductId.TabIndex = 1;
             // 
             // btnList
             // 
@@ -72,6 +78,7 @@
             this.btnList.TabIndex = 2;
             this.btnList.Text = "Listele";
             this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
             // 
             // dataGridView1
             // 
@@ -82,13 +89,13 @@
             this.dataGridView1.Size = new System.Drawing.Size(654, 302);
             this.dataGridView1.TabIndex = 3;
             // 
-            // textBox2
+            // txtProductName
             // 
-            this.textBox2.Location = new System.Drawing.Point(96, 62);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 5;
+            this.txtProductName.Location = new System.Drawing.Point(96, 62);
+            this.txtProductName.Multiline = true;
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(100, 20);
+            this.txtProductName.TabIndex = 5;
             // 
             // label2
             // 
@@ -99,13 +106,13 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Ürün Adı:";
             // 
-            // textBox3
+            // txtProductStock
             // 
-            this.textBox3.Location = new System.Drawing.Point(96, 115);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 9;
+            this.txtProductStock.Location = new System.Drawing.Point(96, 115);
+            this.txtProductStock.Multiline = true;
+            this.txtProductStock.Name = "txtProductStock";
+            this.txtProductStock.Size = new System.Drawing.Size(100, 20);
+            this.txtProductStock.TabIndex = 9;
             // 
             // label3
             // 
@@ -116,13 +123,13 @@
             this.label3.TabIndex = 8;
             this.label3.Text = "Stok:";
             // 
-            // textBox4
+            // txtProductPrice
             // 
-            this.textBox4.Location = new System.Drawing.Point(96, 89);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 7;
+            this.txtProductPrice.Location = new System.Drawing.Point(96, 89);
+            this.txtProductPrice.Multiline = true;
+            this.txtProductPrice.Name = "txtProductPrice";
+            this.txtProductPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtProductPrice.TabIndex = 7;
             // 
             // label4
             // 
@@ -133,13 +140,13 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "Fiyat:";
             // 
-            // textBox5
+            // txtProductCategory
             // 
-            this.textBox5.Location = new System.Drawing.Point(96, 141);
-            this.textBox5.Multiline = true;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 11;
+            this.txtProductCategory.Location = new System.Drawing.Point(96, 141);
+            this.txtProductCategory.Multiline = true;
+            this.txtProductCategory.Name = "txtProductCategory";
+            this.txtProductCategory.Size = new System.Drawing.Size(100, 20);
+            this.txtProductCategory.TabIndex = 11;
             // 
             // label5
             // 
@@ -150,56 +157,130 @@
             this.label5.TabIndex = 10;
             this.label5.Text = "Kategori:";
             // 
-            // button2
+            // btnAdd
             // 
-            this.button2.Location = new System.Drawing.Point(96, 221);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 35);
-            this.button2.TabIndex = 12;
-            this.button2.Text = "Ekle";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnAdd.Location = new System.Drawing.Point(96, 221);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 35);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
-            // button3
+            // btnUpdate
             // 
-            this.button3.Location = new System.Drawing.Point(96, 303);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 35);
-            this.button3.TabIndex = 14;
-            this.button3.Text = "Güncelle";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnUpdate.Location = new System.Drawing.Point(96, 303);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 35);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
-            // button4
+            // btnDelete
             // 
-            this.button4.Location = new System.Drawing.Point(96, 262);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(100, 35);
-            this.button4.TabIndex = 13;
-            this.button4.Text = "Sil";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnDelete.Location = new System.Drawing.Point(96, 262);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 35);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(91, 381);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(208, 25);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Toplam Kitap Sayısı:";
+            // 
+            // lblProductTotalCount
+            // 
+            this.lblProductTotalCount.AutoSize = true;
+            this.lblProductTotalCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblProductTotalCount.ForeColor = System.Drawing.Color.Yellow;
+            this.lblProductTotalCount.Location = new System.Drawing.Point(305, 381);
+            this.lblProductTotalCount.Name = "lblProductTotalCount";
+            this.lblProductTotalCount.Size = new System.Drawing.Size(24, 25);
+            this.lblProductTotalCount.TabIndex = 16;
+            this.lblProductTotalCount.Text = "0";
+            // 
+            // lblMostExpensiveProduct
+            // 
+            this.lblMostExpensiveProduct.AutoSize = true;
+            this.lblMostExpensiveProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMostExpensiveProduct.ForeColor = System.Drawing.Color.Yellow;
+            this.lblMostExpensiveProduct.Location = new System.Drawing.Point(305, 415);
+            this.lblMostExpensiveProduct.Name = "lblMostExpensiveProduct";
+            this.lblMostExpensiveProduct.Size = new System.Drawing.Size(24, 25);
+            this.lblMostExpensiveProduct.TabIndex = 18;
+            this.lblMostExpensiveProduct.Text = "0";
+            this.lblMostExpensiveProduct.Click += new System.EventHandler(this.lblMostExpensiveProduct_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(134, 415);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(165, 25);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "En Pahalı Kitap:";
+            // 
+            // lblLeastExpensiveProduct
+            // 
+            this.lblLeastExpensiveProduct.AutoSize = true;
+            this.lblLeastExpensiveProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblLeastExpensiveProduct.ForeColor = System.Drawing.Color.Yellow;
+            this.lblLeastExpensiveProduct.Location = new System.Drawing.Point(305, 450);
+            this.lblLeastExpensiveProduct.Name = "lblLeastExpensiveProduct";
+            this.lblLeastExpensiveProduct.Size = new System.Drawing.Size(24, 25);
+            this.lblLeastExpensiveProduct.TabIndex = 20;
+            this.lblLeastExpensiveProduct.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(145, 450);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(154, 25);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "En Ucuz Kitap:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.ClientSize = new System.Drawing.Size(1053, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox5);
+            this.ClientSize = new System.Drawing.Size(1053, 519);
+            this.Controls.Add(this.lblLeastExpensiveProduct);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lblMostExpensiveProduct);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblProductTotalCount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtProductCategory);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtProductStock);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtProductPrice);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtProductName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnList);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtProductId);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -209,20 +290,26 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtProductId;
         private System.Windows.Forms.Button btnList;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtProductName;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtProductStock;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtProductPrice;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtProductCategory;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblProductTotalCount;
+        private System.Windows.Forms.Label lblMostExpensiveProduct;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblLeastExpensiveProduct;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- CSharpEgitimKampi501/Form1.cs
+++ CSharpEgitimKampi501/Form1.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CSharpEgitimKampi501.Dtos;
+using Dapper;
+using System;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace CSharpEgitimKampi501
@@ -17,7 +13,79 @@ namespace CSharpEgitimKampi501
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        SqlConnection connection = new SqlConnection("Server=EMIRHAN\\SQLEXPRESS;initial Catalog=EgitimKampi501Db; integrated security=true;");
+
+        private async void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private async void btnList_Click(object sender, EventArgs e)
+        {
+            string query = "Select * From TblProduct";
+            var values = await connection.QueryAsync<ResultProductDto>(query);
+            dataGridView1.DataSource = values;
+        }
+
+        // Change the signature of btnAdd_Click to match the expected EventHandler delegate
+        private async void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            string query = "insert into TblProduct (ProductName, ProductStock, ProductPrice, ProductCategory) values (@productName, @productStock, @productPrice, @productCategory)";
+
+            var parametres = new DynamicParameters();
+
+            parametres.Add("@productName", txtProductName.Text);
+
+            parametres.Add("@productStock", txtProductStock.Text);
+
+            parametres.Add("@productPrice", txtProductPrice.Text);
+
+            parametres.Add("@productCategory", txtProductCategory.Text);
+
+            await connection.ExecuteAsync(query, parametres);
+
+            MessageBox.Show("Ürün eklendi.");
+
+        }
+
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            string query = "Delete From TblProduct where ProductId=@productId";
+            var parametres = new DynamicParameters();
+            parametres.Add("@productId", txtProductId.Text);
+            await connection.ExecuteAsync(query, parametres);
+            MessageBox.Show("Ürün silindi.");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string query = "update TblProduct Set ProductName=@productName, ProductStock=@productStock, ProductPrice=@productPrice, ProductCategory=@productCategory where ProductId=@productId";
+            var parametres = new DynamicParameters();
+            parametres.Add("@productId", txtProductId.Text);
+            parametres.Add("@productName", txtProductName.Text);
+            parametres.Add("@productStock", txtProductStock.Text);
+            parametres.Add("@productPrice", txtProductPrice.Text);
+            parametres.Add("@productCategory", txtProductCategory.Text);
+            connection.ExecuteAsync(query, parametres);
+            MessageBox.Show("Ürün güncellendi.");
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            string query = "Select Count(*) From TblProduct";
+            var productTotalCount = await connection.QueryAsync<int>(query);
+            lblProductTotalCount.Text = productTotalCount.AsList()[0].ToString();
+
+            string query2 = "Select ProductName from TblProduct Where ProductPrice= (Select Max(ProductPrice) From TblProduct)";
+            var mostExpensiveProduct = await connection.QueryAsync<string>(query2);
+            lblMostExpensiveProduct.Text = mostExpensiveProduct.AsList()[0];
+
+            string query3 = "Select ProductName from TblProduct Where ProductPrice= (Select Min(ProductPrice) From TblProduct)";
+            var leastExpensiveProduct = await connection.QueryAsync<string>(query3);
+            lblLeastExpensiveProduct.Text = leastExpensiveProduct.AsList()[0];
+        }
+
+        private void lblMostExpensiveProduct_Click(object sender, EventArgs e)
         {
 
         }
